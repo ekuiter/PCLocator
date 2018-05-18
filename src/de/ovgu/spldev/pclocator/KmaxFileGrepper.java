601481a47f5aa3d032f71ceb860098b387c9c4c7
Elias Kuiter <info@elias-kuiter.de>
2018-05-18 20:47:57 +0200

    Consider subdir_pcs in Kmax files

--- src/de/ovgu/spldev/pclocator/KmaxFileGrepper.java
+++ src/de/ovgu/spldev/pclocator/KmaxFileGrepper.java
@@ -3,11 +3,12 @@ package de.ovgu.spldev.pclocator;
 import com.google.common.escape.Escaper;
 import com.google.common.escape.Escapers;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Gets presence conditions from a Kmax result file. Requires a shell with basic commands (cat, grep, cut),
@@ -16,7 +17,7 @@ import java.nio.file.Paths;
  */
 public class KmaxFileGrepper {
     private static Escaper escaper;
-    private PresenceCondition kmaxPresenceCondition;
+    private PresenceCondition[] kmaxPresenceConditions;
 
     static {
         Escapers.Builder builder = Escapers.builder();
@@ -25,38 +26,40 @@ public class KmaxFileGrepper {
     }
 
     public KmaxFileGrepper(PresenceConditionLocator.Implementation implementation, String kmaxFilePath, String projectRootPath, String filePath) {
-        kmaxPresenceCondition = locatePresenceCondition(implementation, Paths.get(kmaxFilePath), Paths.get(projectRootPath), Paths.get(filePath));
+        kmaxPresenceConditions = locatePresenceConditions(implementation, Paths.get(kmaxFilePath), Paths.get(projectRootPath), Paths.get(filePath));
     }
 
-    private String run(String... args) {
-        StringBuilder output = new StringBuilder();
-        ProcessBuilder pb = new ProcessBuilder(args);
+    private ArrayList<String> searchKmaxFile(Path kmaxFilePath, Path objectFile) {
+        ArrayList<String> componentSearches = new ArrayList<>();
+        componentSearches.add("unit_pc " + objectFile + " ");
+        for (Path component = objectFile.getParent(); component != null; component = component.getParent())
+            componentSearches.add("subdir_pc " + component + " ");
+        ArrayList<String> results = new ArrayList<>();
 
         try {
-            Process p = pb.start();
-            p.waitFor();
-            if (p.exitValue() != 0)
-                throw new RuntimeException("exit value was " + p.exitValue());
-            BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
-
-            String line;
-            while ((line = reader.readLine()) != null)
-                output.append(line).append("\n");
-        } catch (Exception e) {
+            Files.lines(kmaxFilePath)
+                    .filter(line -> {
+                        for (String componentSearch : componentSearches)
+                            if (line.startsWith(componentSearch)) {
+                                componentSearches.remove(componentSearch); // found component, don't keep searching
+                                return true;
+                            }
+                        return false;
+                    })
+                    .map(line -> {
+                        String[] parts = line.split(" ");
+                        return String.join(" ", Arrays.copyOfRange(parts, 2, parts.length));
+                    })
+                    .forEach(results::add);
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
-        return output.toString().trim();
+        return results;
     }
 
-    private String runInShell(String command, String... args) {
-        for (int i = 0; i < args.length; i++)
-            args[i] = escaper.escape(args[i]);
-        return run("/bin/sh", "-c", String.format(command, (Object[]) args));
-    }
-
-    private PresenceCondition locatePresenceCondition(PresenceConditionLocator.Implementation implementation,
-                                                      Path kmaxFilePath, Path projectRootPath, Path filePath) {
+    private PresenceCondition[] locatePresenceConditions(PresenceConditionLocator.Implementation implementation,
+                                                             Path kmaxFilePath, Path projectRootPath, Path filePath) {
         if (!filePath.toString().endsWith(".c"))
             throw new RuntimeException("file " + filePath + " is not a C file");
         if (!Files.exists(filePath))
@@ -64,15 +67,16 @@ public class KmaxFileGrepper {
         if (!filePath.startsWith(projectRootPath))
             throw new RuntimeException("project root " + projectRootPath + " does not contain " + filePath);
 
-        String search = "^unit_pc " + projectRootPath.relativize(filePath).toString().replaceAll("\\.c$", ".o") + " ";
-        String result = runInShell("cat '%s' | grep '%s' | cut -d ' ' -f 3-", kmaxFilePath.toAbsolutePath().toString(), search);
+        Path objectFile = Paths.get(projectRootPath.relativize(filePath).toString().replaceAll("\\.c$", ".o"));
 
-        if (result.length() == 0)
-            return PresenceCondition.NOT_FOUND;
-        return implementation.fromDNF(result);
+        // Both unit_pc and subdir_pc are unique if present, but there might be a presence condition
+        // for every single path component - find them and transform the DNF formulas into presence conditions.
+        return searchKmaxFile(kmaxFilePath, objectFile).stream().map(implementation::fromDNF).toArray(PresenceCondition[]::new);
     }
 
     public PresenceCondition modifyPresenceCondition(PresenceCondition presenceCondition) {
-        return kmaxPresenceCondition.and(presenceCondition);
+        for (int i = kmaxPresenceConditions.length - 1; i >= 0; i--)
+            presenceCondition = kmaxPresenceConditions[i].and(presenceCondition);
+        return presenceCondition;
     }
 }
